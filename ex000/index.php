8bed0cca70e7542ccc174656baf206b3978f7f9b
laercioss <71737006+laercioss@users.noreply.github.com>
2023-08-22 15:43:58 -0300

    Curso php

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá mundo ! \u{1f30e}";
+        ?>
+    </h1>
+    <p>Começo de programação PHP</p>
+</body>
+</html>
---
+++ ex001/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <title>Informações PHP</title>
+</head>
+<body>
+    <h1>Dados do servidor PHP</h1>
+    <?php 
+        phpinfo();
+    ?>
+</body>
+</html>
