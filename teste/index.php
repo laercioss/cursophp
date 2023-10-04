90d6e8d204f71c754d64139f0077447a9b49d557
laercioss <71737006+laercioss@users.noreply.github.com>
2023-10-04 08:00:13 -0300

    atualização

---
+++ teste/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exercicio</title>
+    </head>
+    <body>
+        <header>
+            Teste de conhecimento
+        </header>
+        <section>
+            <form action="" method="get"></form>
+        </section>
+    </body>
+</html>
